ce5edc53ca30ad44d8cbeac4e2e0fe4fdc377e6b
jackyzhou <jackyzhou@lean-soft.cn>
2017-11-22 16:20:32 +0800

    update

--- docker-elk-server/labs/scripts/elk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh   
-cd /home/leansoft                                                                        
-git clone https://github.com/lean-soft/elk.git                                      
-cd /home/leansoft/elk                                                               
-docker-compose -f docker-elk-only.yml up -d
